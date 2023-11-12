350d74d5f3b119975df0934953b979903c8dd325
Vaibhav Kshirsagar <maulikshirsagarvk18@gmail.com>
2023-11-12 16:27:28 +0530

    creating sign up ui and update background color

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -20,13 +20,13 @@ export default function Header() {
           </form> 
           <ul className="flex gap-4">
                 <Link to={"/"}>
-                    <li className="hidden sm:inline text-slate-700">Home</li>
+                    <li className="hidden sm:inline text-slate-700 hover:underline">Home</li>
                 </Link>
                 <Link to={"/about"}>
-                    <li className="hidden sm:inline text-slate-700">About</li>
+                    <li className="hidden sm:inline text-slate-700 hover:underline">About</li>
                 </Link>
                 <Link to={"/sign-in"}>
-                    <li className="hidden sm:inline text-slate-700">Sign in</li>
+                    <li className="sm:inline text-slate-700 hover:underline">Sign in</li>
                 </Link>
           </ul>
         </div>
--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -1,7 +1,48 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-export default function SingUp() {
+export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setFormData({ ...formData, [id]: value });
+  };
+  
   return (
-    <div>SingUp</div>
-  )
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <form className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="username"
+          className="border p-3 rounded-lg"
+          id="username"
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          placeholder="email"
+          className="border p-3 rounded-lg"
+          id="email"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="border p-3 rounded-lg"
+          id="password"
+          onChange={handleChange}
+        />
+        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+          Sign up
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Have an account?</p>
+        <Link to={"/sign-in"}>
+          <span className="text-blue-700">Sign in</span>
+        </Link>
+      </div>
+    </div>
+  );
 }
