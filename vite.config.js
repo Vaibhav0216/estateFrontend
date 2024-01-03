8ff8989b762ae6b18fc15e4e7baa2cf259791526
Vaibhav Kshirsagar <maulikshirsagarvk18@gmail.com>
2024-01-03 20:57:29 +0530

    repace localhost url by backend url

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'http://localhost:8080',
+      '/api': 'https://backend-v6pj.onrender.com',
       secure: false,
     },
   },
