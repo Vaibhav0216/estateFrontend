131bcbecf53c636a025251c5c16175b36485b8a6
Vaibhav Kshirsagar <maulikshirsagarvk18@gmail.com>
2024-01-03 14:58:27 +0530

    compenent c->C

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import ListingItem from '../Components/ListingItem'; 
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation } from 'swiper/modules';
 import SwiperCore from 'swiper';
 import 'swiper/css/bundle';
-import ListingItem from '../components/ListingItem';
 
 export default function Home() {
   const [offerListings, setOfferListings] = useState([]);
@@ -13,7 +13,7 @@ export default function Home() {
   SwiperCore.use([Navigation]);
   console.log(offerListings);
   useEffect(() => {
-    const fetchOfferListings = async () => {
+    const fetchOfferListings = async () => { 
       try {
         const res = await fetch('/api/listing/get?offer=true&limit=4');
         const data = await res.json();
