917dd729f96fa6128e442daccbcdafcd9b0e9b6e
Vaibhav Kshirsagar <maulikshirsagarvk18@gmail.com>
2024-01-06 18:51:16 +0530

    localhost 8080 introduce for testing

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'https://backend-v6pj.onrender.com',
-      secure: false,
+      '/api': {
+        target: 'http://localhost:8080',
+        secure: false,
+      },
     },
-    host: true,
-    strictPort: true,
-    port: 8000,
   },
+
   plugins: [react()],
-  
-})
+}); 
